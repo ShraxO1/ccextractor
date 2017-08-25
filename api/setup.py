58eaaf6a93c35b4d04c901c3f6e7e4371965770b
Diptanshu8 <diptanshuj@gmail.com>
2017-08-25 04:11:12 +0000

    added basic Manifest file and setup.py scripts in api/ directory

---
+++ api/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup,Extension
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+import subprocess
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        subprocess.check_call(['./build_library'])
+        develop.run(self)
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        subprocess.check_call(['./build_library'])
+        install.run(self)
+
+setup(
+       name='ccextractor',
+       version = '0.1',
+       author      = "Skrill",
+       description = "Testing setup script for generating the module",
+       cmdclass={
+           'develop': PostDevelopCommand,
+           'install':PostInstallCommand,
+           },
+)
