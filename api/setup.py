d3ca2da2b684f0827abe1fc1ff568bda97dcb476
Diptanshu8 <diptanshuj@gmail.com>
2017-08-26 14:12:32 +0000

    writing code for class implementation in callback mechanism

--- api/setup.py
+++ api/setup.py
@@ -2,17 +2,29 @@ from distutils.core import setup,Extension
 from setuptools.command.develop import develop
 from setuptools.command.install import install
 import subprocess
+import os
+import shutil
 
 class PostDevelopCommand(develop):
     """Post-installation for development mode."""
     def run(self):
-        subprocess.check_call(['./build_library'])
+        path = os.path.join(os.getcwd(),"package_build_scripts")
+        files = [os.path.join(path , item) for item in os.listdir(path)]
+        for f in files:
+            shutil.move(f,os.getcwd())
+
+        subprocess.check_call(['./build_library_package'])
         develop.run(self)
 
 class PostInstallCommand(install):
     """Post-installation for installation mode."""
     def run(self):
-        subprocess.check_call(['./build_library'])
+        path = os.path.join(os.getcwd(),"package_build_scripts")
+        files = [os.path.join(path , item) for item in os.listdir(path)]
+        for f in files:
+            shutil.move(f,os.getcwd())
+
+        subprocess.check_call(['./build_library_package'])
         install.run(self)
 
 setup(
