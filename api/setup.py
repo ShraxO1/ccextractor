b7c0e143bf3f1d194a77bc4145cfd81ad58a1677
Diptanshu8 <diptanshuj@gmail.com>
2017-08-28 15:48:30 +0000

    pushing on GITHUB. Working for global installations but fails for virtualenv

--- api/setup.py
+++ api/setup.py
@@ -10,38 +10,45 @@ import shutil
 class PostDevelopCommand(develop):
     """Post-installation for development mode."""
     def run(self):
+        path = os.path.join(os.getcwd(),"package_build_scripts")
+        files = [os.path.join(path , item) for item in os.listdir(path)]
+        for f in files:
+            shutil.move(f,os.getcwd())
+        subprocess.check_call(['./build_library_package'])
+        files = [item for item in os.listdir(os.getcwd()) if item=='ccextractor.py' or item =='_ccextractor.so']
+
         develop.run(self)
 
 class PostInstallCommand(install):
     """Post-installation for installation mode."""
     def run(self):
+        path = os.path.join(os.getcwd(),"package_build_scripts")
+        files = [os.path.join(path , item) for item in os.listdir(path)]
+        for f in files:
+            shutil.move(f,os.getcwd())
+        subprocess.check_call(['./build_library_package'])
+        files = [item for item in os.listdir(os.getcwd()) if item=='ccextractor.py' or item =='_ccextractor.so']
         install.run(self)
 
-if not sys.argv[-1]=='sdist':
-    path = os.path.join(os.getcwd(),"package_build_scripts")
-    files = [os.path.join(path , item) for item in os.listdir(path)]
-    for f in files:
-        shutil.move(f,os.getcwd())
-    subprocess.check_call(['./build_library_package'])
-    files = [item for item in os.listdir(os.getcwd()) if item=='ccextractor.py' or item =='_ccextractor.so']
-    setup(
-       name='ccextractor',
-       version = '0.1',
-       author      = "Skrill",
-       description = "Testing setup script for generating the module",
-       packages = ['ccextractor'],
-       #package_dir = {'ccextractor':''},
-       package_dir = {'ccextractor':''},
-        
-       package_data = {'ccextractor':['_ccextractor.so','ccextractor.py']},
-    include_package_data=True,
-       cmdclass={
-           'develop': PostDevelopCommand,
-           'install':PostInstallCommand,
-           },
-    )
-else:
-    setup(
+setup(
+           name='ccextractor',
+           version = '0.1',
+           author      = "Skrill",
+           description = "Testing setup script for generating the module",
+           packages = ['ccextractor'],
+           #package_dir = {'ccextractor':''},
+           package_dir = {'ccextractor':''},
+            
+           package_data = {'ccextractor':['_ccextractor.so','ccextractor.py']},
+        include_package_data=True,
+           cmdclass={
+               'develop': PostDevelopCommand,
+               'install':PostInstallCommand,
+               },
+           )
+"""
+
+setup(
        name='ccextractor',
        version = '0.1',
        author      = "Skrill",
@@ -51,3 +58,4 @@ else:
            'install':PostInstallCommand,
            },
     )
+"""
